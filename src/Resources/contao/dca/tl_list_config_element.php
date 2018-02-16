66c0c8b0ddf8067fcc7cf0ec4379c36d77ca872f
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2018-02-16 14:07:29 +0100

    added tl_list_config_element type_pid index

--- src/Resources/contao/dca/tl_list_config_element.php
+++ src/Resources/contao/dca/tl_list_config_element.php
@@ -15,7 +15,8 @@ $GLOBALS['TL_DCA']['tl_list_config_element'] = [
         ],
         'sql'               => [
             'keys' => [
-                'id' => 'primary'
+                'id'       => 'primary',
+                'type,pid' => 'index'
             ]
         ]
     ],
@@ -55,7 +56,7 @@ $GLOBALS['TL_DCA']['tl_list_config_element'] = [
                 'href'       => 'act=delete',
                 'icon'       => 'delete.gif',
                 'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm']
-                                . '\'))return false;Backend.getScrollOffset()"'
+                    . '\'))return false;Backend.getScrollOffset()"'
             ],
             'show'   => [
                 'label' => &$GLOBALS['TL_LANG']['tl_list_config_element']['show'],
@@ -73,31 +74,31 @@ $GLOBALS['TL_DCA']['tl_list_config_element'] = [
         \HeimrichHannot\ListBundle\Backend\ListConfigElement::TYPE_IMAGE => '{title_type_legend},title,type;{config_legend},imageSelectorField,imageField,imgSize,placeholderImageMode;'
     ],
     'subpalettes' => [
-        'placeholderImageMode_' . \HeimrichHannot\ListBundle\Backend\ListConfigElement::PLACEHOLDER_IMAGE_MODE_SIMPLE   => 'placeholderImage',
+        'placeholderImageMode_' . \HeimrichHannot\ListBundle\Backend\ListConfigElement::PLACEHOLDER_IMAGE_MODE_SIMPLE => 'placeholderImage',
         'placeholderImageMode_'
-        . \HeimrichHannot\ListBundle\Backend\ListConfigElement::PLACEHOLDER_IMAGE_MODE_GENDERED                         => 'genderField,placeholderImage,placeholderImageFemale',
+        . \HeimrichHannot\ListBundle\Backend\ListConfigElement::PLACEHOLDER_IMAGE_MODE_GENDERED                       => 'genderField,placeholderImage,placeholderImageFemale',
     ],
     'fields'      => [
-        'id'                   => [
+        'id'                     => [
             'sql' => "int(10) unsigned NOT NULL auto_increment"
         ],
-        'pid'                  => [
+        'pid'                    => [
             'foreignKey' => 'tl_list_config.title',
             'sql'        => "int(10) unsigned NOT NULL default '0'",
             'relation'   => ['type' => 'belongsTo', 'load' => 'eager']
         ],
-        'tstamp'               => [
+        'tstamp'                 => [
             'label' => &$GLOBALS['TL_LANG']['tl_list_config_element']['tstamp'],
             'sql'   => "int(10) unsigned NOT NULL default '0'"
         ],
-        'dateAdded'            => [
+        'dateAdded'              => [
             'label'   => &$GLOBALS['TL_LANG']['MSC']['dateAdded'],
             'sorting' => true,
             'flag'    => 6,
             'eval'    => ['rgxp' => 'datim', 'doNotCopy' => true],
             'sql'     => "int(10) unsigned NOT NULL default '0'"
         ],
-        'title'                => [
+        'title'                  => [
             'label'     => &$GLOBALS['TL_LANG']['tl_list_config_element']['title'],
             'exclude'   => true,
             'search'    => true,
@@ -105,7 +106,7 @@ $GLOBALS['TL_DCA']['tl_list_config_element'] = [
             'eval'      => ['maxlength' => 255, 'tl_class' => 'w50', 'mandatory' => true],
             'sql'       => "varchar(255) NOT NULL default ''"
         ],
-        'type'                 => [
+        'type'                   => [
             'label'     => &$GLOBALS['TL_LANG']['tl_list_config_element']['type'],
             'exclude'   => true,
             'filter'    => true,
@@ -115,30 +116,28 @@ $GLOBALS['TL_DCA']['tl_list_config_element'] = [
             'eval'      => ['tl_class' => 'w50', 'mandatory' => true, 'includeBlankOption' => true, 'submitOnChange' => true],
             'sql'       => "varchar(64) NOT NULL default ''"
         ],
-        'imageSelectorField'   => [
+        'imageSelectorField'     => [
             'label'            => &$GLOBALS['TL_LANG']['tl_list_config_element']['imageSelectorField'],
             'inputType'        => 'select',
-            'options_callback' => function (DataContainer $dc)
-            {
+            'options_callback' => function (DataContainer $dc) {
                 return \HeimrichHannot\ListBundle\Util\ListConfigElementHelper::getCheckboxFields($dc);
             },
             'exclude'          => true,
             'eval'             => ['includeBlankOption' => true, 'mandatory' => true, 'chosen' => true, 'tl_class' => 'w50 autoheight'],
             'sql'              => "varchar(64) NOT NULL default ''",
         ],
-        'imageField'           => [
+        'imageField'             => [
             'label'            => &$GLOBALS['TL_LANG']['tl_list_config_element']['imageField'],
             'inputType'        => 'select',
-            'options_callback' => function (DataContainer $dc)
-            {
+            'options_callback' => function (DataContainer $dc) {
                 return \HeimrichHannot\ListBundle\Util\ListConfigElementHelper::getFields($dc);
             },
             'exclude'          => true,
             'eval'             => ['includeBlankOption' => true, 'mandatory' => true, 'chosen' => true, 'tl_class' => 'w50 autoheight'],
             'sql'              => "varchar(64) NOT NULL default ''",
         ],
-        'imgSize'              => $GLOBALS['TL_DCA']['tl_module']['fields']['imgSize'],
-        'placeholderImageMode' => [
+        'imgSize'                => $GLOBALS['TL_DCA']['tl_module']['fields']['imgSize'],
+        'placeholderImageMode'   => [
             'label'     => &$GLOBALS['TL_LANG']['tl_list_config_element']['placeholderImageMode'],
             'exclude'   => true,
             'filter'    => true,
@@ -148,25 +147,24 @@ $GLOBALS['TL_DCA']['tl_list_config_element'] = [
             'eval'      => ['tl_class' => 'w50', 'includeBlankOption' => true, 'submitOnChange' => true],
             'sql'       => "varchar(64) NOT NULL default ''"
         ],
-        'placeholderImage'     => [
+        'placeholderImage'       => [
             'label'     => &$GLOBALS['TL_LANG']['tl_list_config_element']['placeholderImage'],
             'exclude'   => true,
             'inputType' => 'fileTree',
             'eval'      => ['tl_class' => 'w50 autoheight', 'fieldType' => 'radio', 'filesOnly' => true, 'extensions' => Config::get('validImageTypes'), 'mandatory' => true],
             'sql'       => "binary(16) NULL"
         ],
-        'placeholderImageFemale'     => [
+        'placeholderImageFemale' => [
             'label'     => &$GLOBALS['TL_LANG']['tl_list_config_element']['placeholderImageFemale'],
             'exclude'   => true,
             'inputType' => 'fileTree',
             'eval'      => ['tl_class' => 'w50 autoheight', 'fieldType' => 'radio', 'filesOnly' => true, 'extensions' => Config::get('validImageTypes'), 'mandatory' => true],
             'sql'       => "binary(16) NULL"
         ],
-        'genderField'           => [
+        'genderField'            => [
             'label'            => &$GLOBALS['TL_LANG']['tl_list_config_element']['genderField'],
             'inputType'        => 'select',
-            'options_callback' => function (DataContainer $dc)
-            {
+            'options_callback' => function (DataContainer $dc) {
                 return \HeimrichHannot\ListBundle\Util\ListConfigElementHelper::getFields($dc);
             },
             'exclude'          => true,

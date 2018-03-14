1334d0fc6c36fa182d9aa2a162fe2e96a24d9967
Dennis Patzer <d.patzer@heimrich-hannot.de>
2018-03-14 17:55:57 +0100

    fixed json

--- src/Item/DefaultItem.php
+++ src/Item/DefaultItem.php
@@ -373,9 +373,7 @@ class DefaultItem implements ItemInterface, \JsonSerializable
      */
     public function jsonSerialize()
     {
-        $data = $this->getFormatted();
-
-        return array_merge($data, System::getContainer()->get('huh.utils.class')->jsonSerialize(static::class, $this));
+        return array_merge($data, System::getContainer()->get('huh.utils.class')->jsonSerialize(static::class, $this, $this->getFormatted()));
     }
 
     public function generateIdOrAlias(ItemInterface $item, ListConfigModel $listConfig): string

75697d83e55876a806e1fe9f53d19c9ae03999a0
Thomas Körner <t.koerner@heimrich-hannot.de>
2018-06-12 09:03:13 +0200

    added property comments to listconfigelement model

--- src/Model/ListConfigElementModel.php
+++ src/Model/ListConfigElementModel.php
@@ -10,6 +10,21 @@ namespace HeimrichHannot\ListBundle\Model;
 
 use Contao\Model;
 
+/**
+ * Class ListConfigElementModel.
+ *
+ * @property int    $id
+ * @property int    $pid
+ * @property int    $tstamp
+ * @property int    $dateAdded
+ * @property string $title
+ * @property string $type
+ * @property string $imageSelectorField
+ * @property string $imgSize
+ * @property string $placeholderImage
+ * @property string $placeholderImageFemale
+ * @property string $genderField
+ */
 class ListConfigElementModel extends Model
 {
     protected static $strTable = 'tl_list_config_element';

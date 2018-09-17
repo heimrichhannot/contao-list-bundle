7330910f3241e60610c4fdd572de56200af59c54
Thomas Körner <t.koerner@heimrich-hannot.de>
2018-09-17 09:04:40 +0200

    updated composer.json, updated travis, added tests

---
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Copyright (c) 2018 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace HeimrichHannot\ListBundle\Test\DependenyInjection;
+
+use Contao\TestCase\ContaoTestCase;
+use HeimrichHannot\ListBundle\DependencyInjection\Configuration;
+
+class ConfigurationTest extends ContaoTestCase
+{
+    public function testGetConfigTreeBuilder()
+    {
+        $configuration = new Configuration(false);
+
+        $tree = $configuration->getConfigTreeBuilder();
+
+        $root = $tree->buildTree();
+
+        $this->assertSame('huh', $root->getName());
+
+        $level1 = $root->getChildren();
+
+        $this->assertCount(1, $level1);
+
+        $listNode = $level1['list'];
+
+        $level2 = $listNode->getChildren();
+
+        $this->assertCount(5, $level2);
+    }
+}
---
+++ tests/DependencyInjection/ListExtensionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Copyright (c) 2018 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace HeimrichHannot\ListBundle\Test\DependenyInjection;
+
+use Contao\TestCase\ContaoTestCase;
+use HeimrichHannot\ListBundle\DependencyInjection\ListExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+
+class ListExtensionTest extends ContaoTestCase
+{
+    public function testGetAlias()
+    {
+        $extension = new ListExtension();
+        $this->assertSame('huh_list', $extension->getAlias());
+    }
+
+    public function testLoad()
+    {
+        $extension = new ListExtension();
+        $container = new ContainerBuilder(new ParameterBag(['kernel.debug' => false]));
+
+        $extension->load([], $container);
+
+        $this->assertTrue($container->hasDefinition('huh.list.datacontainer.module'));
+        $this->assertTrue($container->hasDefinition('huh.list.util.manager'));
+        $this->assertTrue($container->hasDefinition('huh.list.list-config-registry'));
+        $this->assertTrue($container->hasDefinition('huh.list.listener.search'));
+    }
+}

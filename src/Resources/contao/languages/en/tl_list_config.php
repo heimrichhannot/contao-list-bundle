3bff444c297934b640ee6e7351f91a00061aecfd
Thomas Körner <t.koerner@heimrich-hannot.de>
2022-12-16 14:50:42 +0100

    fixed translations

--- src/Resources/contao/languages/en/tl_list_config.php
+++ src/Resources/contao/languages/en/tl_list_config.php
@@ -6,8 +6,6 @@
  * @license LGPL-3.0-or-later
  */
 
-use HeimrichHannot\ListBundle\ListExtension\DcMultilingualListExtension;
-
 $lang = &$GLOBALS['TL_LANG']['tl_list_config'];
 
 /*
@@ -19,10 +17,8 @@ $lang['listContextVariables'][0] = 'Custom variables';
 $lang['listContextVariables'][1] = 'Enter custom variables that can be used in templates and events.';
 
 // Extensions
-$lang['use'.DcMultilingualListExtension::getAlias()] = [
-    'Add support for "DC_Multilingual"',
-    'Choose this option if the linked entity is translatable through "terminal42/contao-DC_Multilingual".',
-];
+$lang['useDcMultilingual'][0] = 'Add support for "DC_Multilingual"';
+$lang['useDcMultilingual'][0] = 'Choose this option if the linked entity is translatable through "terminal42/contao-DC_Multilingual".';
 $lang['dcMultilingualUseFallbackLang'][0] = 'Use Fallback language';
 $lang['dcMultilingualUseFallbackLang'][1] = 'Choose this option if entities should be displayed in the fallback language if they are not available in the current language. Otherwise only translated records will be displayed';
 
